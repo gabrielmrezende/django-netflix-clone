c2dbb2c87753a50be9f1b982a0e8aad96e1cc49d
Gabriel Rezende <gabrielrezendee03@gmail.com>
2026-03-19 23:15:13 -0300

    feat: adicionando novas funcionalidades

--- filme/urls.py
+++ filme/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from .views import Homefilmes, Homepage, Detalhesfilme
+
+app_name = 'filme'
 
 urlpatterns = [
-    path('', views.homepage),
+    path('', Homepage.as_view(), name='homepage'),
+    path('filmes/', Homefilmes.as_view(), name='homefilmes'),
+    path('filmes/<int:pk>', Detalhesfilme.as_view(), name='detalhesfilme')
 ]
--- filme/views.py
+++ filme/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
+from .models import Filme
+from django.views.generic import TemplateView, ListView, DetailView
 
 
+class Homepage(TemplateView):
+    template_name = "homepage.html"
 
-def homepage(request):
-    return render(request, "homepage.html")
+
+class Homefilmes(ListView):
+    template_name = 'homefilmes.html'
+    model = Filme
+
+class Detalhesfilme(DetailView):
+    template_name = "detalhesfilme.html"
+    model = Filme
+    
+
+
+# def homepage(request):
+#     return render(request, "homepage.html")
+
+
+# def homepage(request):
+#     return render(request, "homepage.html")
